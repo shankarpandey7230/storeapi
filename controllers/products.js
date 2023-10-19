210085909c406cae06868e96dc73216b506190de
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-10-19 14:25:15 +1100

    connecting to database, controllers and router setup with postman

---
+++ controllers/products.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const getAllProductsStatic = async (req, res) => {
+  res.status(200).json({ msg: "product testing route" });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products  route" });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
