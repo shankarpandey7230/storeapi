eb80ab1502add09b3ed442bf688ed5af874ee3a9
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-10-20 23:34:43 +1100

    Mongoose sort filter

--- controllers/products.js
+++ controllers/products.js
@@ -4,15 +4,13 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   // throw new Error("testing async errors");
   const search = "a";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({}).sort("-name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query);
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -23,8 +21,16 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    // console.log(sort);
+    const sortList = sort.split(",").join("");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
